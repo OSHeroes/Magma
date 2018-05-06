10e1f802f99032204ad9ce50c3d9135afd6bf4b1
Synaxis <01xtor@gmail.com>
2018-05-06 06:54:19 -0300

    More routes

--- server/routing.go
+++ server/routing.go
@@ -22,10 +22,54 @@ func (s *Server) registerRoutes() http.Handler {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+//  'nucleus/entitlements/%I64d',0
+//  'ofb/products',0
+//  'nucleus/wallets/%I64d',0
+//  'ofb/purchase/%I64d/%s',0
+//  'nucleus/wallets/%I64d/%s/%d/%s',0
+//  'nucleus/refundAbilities/%I64d',0
+//  'relationships/acknowledge/nucleus:%I64d/%I64d',0
+//  'relationships/acknowledge/server:%s/%I64d',0
+//  'relationships/increase/nucleus:%I64d/nucleus:%I64d/%s',0
+//  'relationships/increase/nucleus:%I64d/server:%s/%s',0
+//  'relationships/increase/server:%s/nucleus:%I64d/%s',0
+//  'relationships/decrease/nucleus:%I64d/nucleus:%I64d/%s',0
+//  'relationships/decrease/nucleus:%I64d/server:%s/%s',0
+//  'relationships/decrease/server:%s/nucleus:%I64d/%s',0
+//  'relationships/status/nucleus:%I64d',0
+//  '<update>',0Ah,0
+//  '</status>',0Ah,0
+//  09h,'<status>',0
+//  '</update>',0
+//  'relationships/status/server:%s',0
+//  '<update>',0Ah,0
+//  '</status>',0Ah,0
+//  09h,'<status>',0
+//  '</name>',0Ah,0
+//  09h,'<name>',0
+//  '</update>',0
+//  'nucleus/personas/%s',0
+//  'relationships/roster/nucleus:%I64d',0
+//  'relationships/roster/server:%s/bvip/1,3',0
+//  'relationships/roster/nucleus:%I64d',0
+//  'relationships/roster/server:%s',0
+// 'user/updateUserProfile/%I64d',0
+//  'nucleus/entitlement/%s/useCount/%d',0
+// : 'nucleus/entitlement/%s/status/%s',0
+// 'user',0
+//  'persona',0
+//  'nucleus/check/%s/%I64d',0
+//  'nucleus/authToken',0
+//  'nucleus/name/%I64d',0
+//  'dc/submit',0
+//  'nucleus/entitlements/%I64d',0
+//  'nucleus/entitlements/%I64d?entitlementTag=%s',0
+
+	// TODO: user/updateUserProfile/%d
 	// r.Use(middleware.Timeout(60 * time.Second))
 	// r.Mount("/debug", middleware.Profiler())
 
-	// TODO: user/updateUserProfile/%d
+
 
 	// Nuclues (authentication and account data)
 	r.Route("/relationships", relationships.New(s.rdr).Routing)
